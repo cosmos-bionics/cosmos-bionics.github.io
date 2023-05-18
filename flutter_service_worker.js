78e01bf3c8e671f41e41e2d22d9853d12ba54c86
cosmos-bionics <115837616+cosmos-bionics@users.noreply.github.com>
2023-05-18 20:54:51 +0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "favicon.png": "107a9e1198486c8ded5da70ef4975ea5",
-"main.dart.js": "73412147e914a4c841ad84236d1ad691",
+"main.dart.js": "1ad887a0a7e75519d8490511f8e048e2",
 "icons/Icon-192.png": "0d069683bd629ee660f47dfba26783ff",
 "icons/Icon-512.png": "107a9e1198486c8ded5da70ef4975ea5",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
@@ -72,8 +72,8 @@ const RESOURCES = {
 "assets/NOTICES": "4cdb88f3eff6a7eedd555d5e434ef642",
 "assets/FontManifest.json": "354ebdd50fb452b5ca5fc5bd78f848b5",
 "manifest.json": "bb8ec6f391223ec0b7e6aa77b2a4f2ec",
-"index.html": "1da302bcb31cb8cc09329e468a422191",
-"/": "1da302bcb31cb8cc09329e468a422191",
+"index.html": "457ebce145c2629491309bf586596470",
+"/": "457ebce145c2629491309bf586596470",
 "version.json": "365c8cdb36c85659ce96be73e086a8f9"
 };
 
